13c5bd43650beba9c25b594a7ad5b9585711da9d
pddojlida <99636326+pddojlida@users.noreply.github.com>
2024-02-07 19:48:27 +1100

    Update guessTheNumber.c

--- guessTheNumber.c
+++ guessTheNumber.c
@@ -14,11 +14,15 @@ void checkLuck(int);         // Function prototype that takes one input of int t
                              // The checkLuck function puts directly instead of returning a value.
 
 int main(void) { // main function returns an int i.e., a 0, does not take any parameters.
+  
    srand(time(0));   // The srand function uses the time fucntion to seed a random number. 
                      // The time returns the the current time in seconds. 
                      // Then, the rand function generates a sequence from the "seed".
 
    guessTheNumber(); // A call to the guessTheNumber fucntion which 
+
+   puts("Thanks for playing!");
+  
 } // end function main.
 
 
@@ -49,30 +53,31 @@ void guessTheNumber(void) { // guessTheNumber generates numbers between 1 and 10
          guesses++;           // Counts how many guesses the user is taking to guess the correct number.
       }
       
-      puts("Excellent! You guessed the number!"); 
+      puts("Excellent! You guessed the number!\n"); 
       checkLuck(guesses);                      // Calls the checkLuck function to determine how lucky the user was in guessing.
       
-      puts("Would you like to play again?"); // Prompts the user for another game. 
+      puts("Would you like to play again?\n"); // Prompts the user for another game. 
       puts("Please type (Y = Yes, Q = No, thanks, Quit)");
-      scanf("%c", &response); 
+      scanf(" %c", &response); 
       
    } while (response == 'Y');                  // BC Y is a character literal it requires parentheses.
+  
 } // end function guessTheNumber.
 
 
 int correctNumber(int g, int a) {   // isCorrect returns true if g equals a,
                                 // but if g does not equal a, a hint is displayed.
-   if (g == a)
+   if (g > 10) // Reminds the user that they need to guess between 1 and 10.
+      puts("That number is outside of the range you are supposed to guess within!\n");
+  
+  if (g == a)
       return 1;
   
-   if (g > 10) // Reminds the user that they need to guess between 1 and 10.
-      puts("That number is outside of the range you are supposed to guess within!");
-  
    if (g < a)
-      printf( "%s", "Too low. Try again.\n? " );
+      printf("%s", "Too low. Try again.\n" );
      
    else
-      printf( "%s", "Too high. Try again.\n? " );
+      printf("%s", "Too high. Try again.\n" );
 
    return 0;
   
@@ -81,13 +86,13 @@ int correctNumber(int g, int a) {   // isCorrect returns true if g equals a,
 void checkLuck(int guessesNumber) { // Function determines if the user is lucky or not.  
                                    // This is an origianl function added to the program.  
    if (guessesNumber <= 2)
-      return puts("/nYou are very lucky!"
-         "\nBuy a lottery ticket!");
+      return puts("You are very lucky!\n"
+         "Buy a lottery ticket!\n");
    if (guessesNumber >= 3 && guessesNumber <= 6)
-      return puts("/nYou are a little lucky!"
-         "\nMaybe buy a lottery ticket?");
+      return puts("You are a little lucky!\n"
+         "Maybe buy a lottery ticket?\n");
    else if (guessesNumber >= 7)
-      return puts("\nYou are not that lucky! Sorry.");  
+      return puts("You are not that lucky! Sorry.\n");  
   
 } // end function checkLuck.
 
