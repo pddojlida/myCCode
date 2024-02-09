55c43855dd9bd78eb2e3756ade943ff14a0dfced
pddojlida <99636326+pddojlida@users.noreply.github.com>
2024-02-09 20:44:48 +1100

    Create ch02ComparingNumbers.c

---
+++ ch02ComparingNumbers.c
@@ -0,0 +1,59 @@
+// Using if statements, relational 
+// operators, and equality operators
+// The original code used only two numbers, 
+// this code is extended to use three numbers
+
+#include <stdio.h>
+
+// function main begins program execution
+int main(void) {
+
+   printf("Enter three integers, and I will tell you\n");
+   printf("the relationships they satisfy: ");
+
+   int num1; // first number to be read from user
+   int num2; // second number to be read from user
+   int num3; // third number to read from user
+   
+   scanf("%d %d %d", &num1, &num2, &num3); // read two integers
+   
+   if ( num1 == num2 == num3) { // if block 1 checks equality
+      printf("All the numbers are equal\n");
+      printf("%d is equal to %d\n", num1, num2);
+   } 
+   else if (num1 == num2) {
+      printf("%d is equal to %d\n", num1, num2);
+   } 
+   else if (num1 == num3) {
+      printf("%d is equal to %d\n", num1, num3);
+   } 
+   else if (num2 == num3) {
+      printf("%d is equal to %d\n", num2, num3);
+   } // end if block 1
+
+   if (num1 != num2) { // if block 2 checks non-equality
+      printf("%d is not equal to %d\n", num1, num2);
+   } 
+   if (num2 != num3) {
+      printf("%d is not equal to %d\n", num2, num3);
+   } 
+   if (num1 != num3) {
+      printf("%d is not equal to %d\n", num1, num3);
+   }
+
+   if (num1 < num2) { 
+      printf( "%d is less than %d\n", num1, num2 );
+   } // end if
+
+   if ( num1 > num2 ) {
+      printf( "%d is greater than %d\n", num1, num2 );
+   } // end if
+
+   if ( num1 <= num2 ) {
+      printf( "%d is less than or equal to %d\n", num1, num2 );
+   } // end if
+
+   if ( num1 >= num2 ) {
+      printf( "%d is greater than or equal to %d\n", num1, num2 );
+   } // end if
+} // end function main
