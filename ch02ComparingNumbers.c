e236fc4e58b5d5ef8884f5c26e7be7be89459169
pddojlida <99636326+pddojlida@users.noreply.github.com>
2024-02-12 14:09:14 +1100

    Update ch02ComparingNumbers.c

--- ch02ComparingNumbers.c
+++ ch02ComparingNumbers.c
@@ -5,7 +5,7 @@
 
 #include <stdio.h>
 
-// function main begins program execution
+// Function main begins program execution.
 int main(void) {
 
    printf("Enter three integers, and I will tell you\n");
@@ -15,9 +15,9 @@ int main(void) {
    int num2; // second number to be read from user
    int num3; // third number to read from user
    
-   scanf("%d %d %d", &num1, &num2, &num3);  // Read three integers
+   scanf("%d %d %d", &num1, &num2, &num3);  // Read three inumners as ints from the user
    
-   if ( num1 == num2 == num3) {  // if block 1 checks equality
+   if ( num1 == num2 == num3) {  // if block 1 checks equality.
       printf("All the numbers are equal\n");
    } 
    else if (num1 == num2) {
@@ -28,42 +28,37 @@ int main(void) {
    } 
    else if (num2 == num3) {
       printf("%d is equal to %d\n", num2, num3);
-   } // end if block 1
+   } // end of if block 1.
 
    
-   if (num1 != num2) {  // if statements check for non-equality
+   if (num1 != num2) {  // if statements check for is not equal.
       printf("%d is not equal to %d\n", num1, num2);
-   } 
+   } // end if.
    if (num2 != num3) {
       printf("%d is not equal to %d\n", num2, num3);
-   } 
+   } // end if.
    if (num1 != num3) {
       printf("%d is not equal to %d\n", num1, num3);
-   }
+   } // End of if statements check for is no equal.
 
    
-   if (num1 < num2) {  // if statements check for inequality
+   if (num1 < num2) {  // if statements check for inequality.
       printf("%d is less than %d\n", num1, num2);
-   } // end if
-
+   } // end if.
    if (num1 > num2) {
       printf("%d is greater than %d\n", num1, num2);
-   } // end if
-
+   } // end if.
    if (num2 < num3) { 
       printf("%d is less than %d\n", num2, num3);
-   } // end if
-
+   } // end if.
    if (num2 > num3) {
       printf("%d is greater than %d\n", num2, num3);
-   } // end if
-
+   } // end if.
    if (num1 < num3) { 
       printf("%d is less than %d\n", num1, num3);
-   } // end if
-
+   } // end if.
    if (num1 > num3) {
       printf("%d is greater than %d\n", num1, num3);
-   } // end if
+   } // end if statements check for inequality.
  
-} // end function main
+} // End function main.
