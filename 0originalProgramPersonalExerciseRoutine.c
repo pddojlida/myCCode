6c0701d43c5770169d97462d05a9f72ca03c6306
PDDojlida <99636326+pddojlida@users.noreply.github.com>
2024-04-11 10:12:26 +1000

    Create 0originalProgramPersonalExerciseRoutine.c

---
+++ 0originalProgramPersonalExerciseRoutine.c
@@ -0,0 +1,42 @@
+// This is an original program for an exercise routine specifically tailored to me.
+// The program is intended to be an andriod app and has timed rests in between sets
+// and in between exercises.  The exercises are in groups targeting certain muscle groups 
+// and will randomly rotate to give variety.  The user can press "START" to start the program,
+// and "DONE" to indicate that they are "DONE" with the exercise and 
+that the rest timer can turn on, and "END" to end the Exercise early.
+
+// As an extension to the program, I will train a NN to recognise the command phrase "START", "DONE and "END".  
+// As a further extension, the program will call out the exercises and motivational phrases.
+
+#include<stdio.h>
+
+void main() {
+
+create a seq rand 1 to 3 -> use this, and then call
+
+if statements
+
+rand 4 to 7
+
+rand 8 to 12
+
+13, 14, 15
+
+16, 17, 18
+  
+rand 19 to 23
+
+  
+}
+
+  
+// The pseudo code for the program is below
+
+- Press "START" to start the program
+- a 10 second count down timer counts down to the first routine
+- a beep sounds when the timer reaches 0
+- the name of the routine, and the amount of sets and the sets in each rep is printed on the screen
+- the use presses "DONE" on the phone to start the count down
+- a beep sounds when the timer reaches 0
+- next routine details are printed
+- repeats until the routine is done
