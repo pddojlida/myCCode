62515e8c4c36e950bff36a8f5d1ddfd2b03841e1
PDDojlida <99636326+pddojlida@users.noreply.github.com>
2024-04-11 14:41:42 +1000

    Update ch05mathLibraryFun.c

--- ch05mathLibraryFun.c
+++ ch05mathLibraryFun.c
@@ -11,31 +11,38 @@ int main(void){
    // calculates and outputs the square root
    printf("sqrt(%.1f) = %.1f\n", 16.0, sqrt(16.0));
    puts(" ");
-   for (i = 1; i <= sqrt(16.0); i++) {       // Doing the inner loop sqrt(16.0) times
+   for (i = 1; i <= sqrt(16.0); i++) {        // Doing the inner loop sqrt(16.0) times
        for (j = 1; j <= sqrt (16.0); j++) {   // The inner loop prints a # character sqrt(16.0) times
            printf("#");
        }
    puts(" "); // Goes to a new line 
    }
    puts(" ");
-   printf("You can see that the square has 16.0 #'s.  If you count how many #'s are on one side you get the square root.\n");
-
+   printf("You can see that the square has 16.0 #'s. If you count how many #'s are on one side you get the square root.\n");
+   puts(" ");
    // calculates and outputs the cube root
-   printf("Imagine a cube %.1f elements wide, %.1f elements tall, and %.1f elements deep.\nIf you count how many elements are on one edge, you get the cube root.\n", cbrt(27.0), cbrt(27.0), cbrt(27.0));
+   printf("Now, imagine a cube %.1f elements wide, %.1f elements tall, and %.1f elements deep.\nIf you count how many elements are on one edge, you get the cube root.\n", cbrt(27.0), cbrt(27.0), cbrt(27.0));
+   puts(" ");
    
    // calculates and outputs the exponential function e to the x
+  
+   puts("Check out the following output sequence of exponential values and how they are converted back to their intputs.");
+   puts(" ");
+   
    for (k = 1; k <= 9; k++) {
        printf("exp(%d) = %.2f\n", k, exp(k));
        puts(" ");
    }
 
     // calculates and outputs the logarithm (base e), but first generates the exponential value
-    for (l = 1; l <= 9; l++) {
+   for (l = 1; l <= 9; l++) {
        printf("exp(%d) = %.2f\n", l, exp(l));
        puts(" ");
        printf("log(%f) = %.1f\n", exp(l), log(exp(l)));
        puts(" ");
-    }
+   }
+   puts(" ");
+
    
    // calculates and outputs the logarithm (base 10)
    printf("log10(%.1f) = %.1f\n", 1.0, log10(1.0));
