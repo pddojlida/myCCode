b3205bf21d42172db76eedaf39b904d4a76a0629
PDDojlida <99636326+pddojlida@users.noreply.github.com>
2024-04-11 19:54:28 +1000

    Update ch05mathLibraryFun.c

--- ch05mathLibraryFun.c
+++ ch05mathLibraryFun.c
@@ -9,6 +9,9 @@ unsigned int m = 1;
 unsigned int n = 0;
 unsigned int o = 0;
 float nDec = 0.0;
+unsigned int p = 0;
+unsigned int pow1 = 10;
+unsigned int pow2 = 0;
 
 int main(void){
 
@@ -72,6 +75,70 @@ int main(void){
    }
 ///////////////////////////////////////////////////////////////////////////////// HERE   
    // calculates and outputs pow(x, y)
+
+   for (p = 1; p <= 9; p++) {
+      pow1 = pow1 - 1;
+      pow2 = pow2 + 1;
+      printf("pow(%.1d, %.1d) = %.1f\n", pow1, pow2, pow(pow1, pow2));
+      printf("pow(%.1d, %.1d) = %.1f\n", pow2, pow1, pow(pow2, pow1));
+   }
+
+
+   // calculates and outputs fmod(x, y)
+   printf("fmod(%.3f/%.3f) = %.3f\n", 13.657, 2.333, 
+      fmod(13.657, 2.333));
+
+   // calculates and outputs sin(x)
+   printf("sin(%.1f) = %.1f\n", 0.0, sin(0.0));
+   
+   // calculates and outputs cos(x)
+   printf("cos(%.1f) = %.1f\n", 0.0, cos(0.0));
+   
+   // calculates and outputs tan(x)
+   printf("tan(%.1f) = %.1f\n", 0.0, tan(0.0));
+} 
+
+/*
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+   // calculates and outputs the square root
+   printf("sqrt(%.1f) = %.1f\n", 900.0, sqrt(900.0));
+   printf("sqrt(%.1f) = %.1f\n", 9.0, sqrt(9.0));
+   
+   // calculates and outputs the cube root
+   printf("cbrt(%.1f) = %.1f\n", 27.0, cbrt(27.0));
+   printf("cbrt(%.1f) = %.1f\n", -8.0, cbrt(-8.0));
+   
+   // calculates and outputs the exponential function e to the x
+   printf("exp(%.1f) = %f\n", 1.0, exp(1.0));
+   printf("exp(%.1f) = %f\n", 2.0, exp(2.0));
+   
+   // calculates and outputs the logarithm (base e)
+   printf("log(%f) = %.1f\n", 2.718282, log(2.718282));
+   printf("log(%f) = %.1f\n", 7.389056, log(7.389056));
+   
+   // calculates and outputs the logarithm (base 10)
+   printf("log10(%.1f) = %.1f\n", 1.0, log10(1.0));
+   printf("log10(%.1f) = %.1f\n", 10.0, log10(10.0));
+   printf("log10(%.1f) = %.1f\n", 100.0, log10(100.0));
+   
+   // calculates and outputs the absolute value
+   printf("fabs(%.1f) = %.1f\n", 13.5, fabs(13.5));
+   printf("fabs(%.1f) = %.1f\n", 0.0, fabs(0.0));
+   printf("fabs(%.1f) = %.1f\n", -13.5, fabs(-13.5));
+   
+   // calculates and outputs ceil(x)
+   printf("ceil(%.1f) = %.1f\n", 9.2, ceil(9.2));
+   printf("ceil(%.1f) = %.1f\n", -9.8, ceil(-9.8));
+
+   // calculates and outputs floor(x)
+   printf("floor(%.1f) = %.1f\n", 9.2, floor(9.2));
+   printf("floor(%.1f) = %.1f\n", -9.8, floor(-9.8));
+   
+   // calculates and outputs pow(x, y)
    printf("pow(%.1f, %.1f) = %.1f\n", 2.0, 7.0, pow(2.0, 7.0));
    printf("pow(%.1f, %.1f) = %.1f\n", 9.0, 0.5, pow(9.0, 0.5));
    
@@ -89,4 +156,4 @@ int main(void){
    printf("tan(%.1f) = %.1f\n", 0.0, tan(0.0));
 } 
 
-/
+*/
