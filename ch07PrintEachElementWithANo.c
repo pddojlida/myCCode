9ee01f543da2ec11a72fffc0bc417c0dde2295ff
PDDojlida <99636326+pddojlida@users.noreply.github.com>
2024-05-26 09:14:38 +1000

    Update ch07PrintEachElementWithANo.c

--- ch07PrintEachElementWithANo.c
+++ ch07PrintEachElementWithANo.c
@@ -2,8 +2,8 @@
 #define SIZE 80
 char string[SIZE]; // create char array
 
-size_t mystery2(const char *s); // prototype, size_t is an unsigned integer type in C and C++ that is used to represent the size of objects in bytes.
-void mystery3(const char *s);
+size_t mystery2(char *s); // prototype, size_t is an unsigned integer type in C and C++ that is used to represent the size of objects in bytes.
+void mystery3(char *s);
 
 int main(void)
 {
@@ -15,7 +15,7 @@ int main(void)
 } 
 
 // What does this function do?
-size_t mystery2(const char *s) {
+size_t mystery2(char *s) {
    size_t x; // counter
   
    // declare and instantiate counter for loop
@@ -28,13 +28,13 @@ size_t mystery2(const char *s) {
 }
 
 // What does this function do?
-void mystery3(const char *s) {
+void mystery3(char *s) {
    // delcare and instantiate variable for keeping track of the array element numnber
    int num = 1;
    // loop through string and print each letter with a corresponding number, verifying the previous count
    for ( ; *s != '\0'; ++s) {
-     printf("%c %d\n", *s, num++);
-   } 
+     printf("%c %d\n", *s, num++); // The key lesson here is that the s in the loop does increment for the purpose of the printf(), and
+   }                               // *s allows you to access the element of the array via the pointer value, the array being being string[] 
 }
 
 /*
